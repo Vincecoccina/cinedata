508611a0a99a404f4dd4c14cd214cd7fac953552
Vince <v.coccina@orange.fr>
2023-02-18 17:25:40 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import MovieInformations from "./components/MovieInfos/MovieInformations";
+import Actors from "./components/Actors/Actors";
+import Profile from "./components/Profile/Profile";
+import Movies from "./components/Movies/Movies";
+import useStyles from "./components/styles";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-     app
+    <div className={classes.root}>
+      <BrowserRouter>
+        <CssBaseline />
+        <NavBar/>
+        <main className={classes.content}>
+        <div className={classes.toolbar} />
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInformations />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MovieInfos/MovieInformations.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieInformations = () => {
+  return (
+    <div>
+      Movie Informations
+    </div>
+  )
+}
+
+export default MovieInformations;
