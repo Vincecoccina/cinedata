611449ec4689c4a58b729bf0de118ddced475960
Vince <v.coccina@orange.fr>
2023-02-18 18:38:00 +0100

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import { Menu, AccountCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import useStyles from "./styles";
 
 const NavBar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(max-width: 600px)");
+  const theme = useTheme();
+  const isAuth = true;
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: "none" }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          {!isMobile && "Search..."}
+          <div>
+            {!isAuth ? (
+              <Button color="inherit" onClick={() => {}}>
+                LOGIN &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={`/profile/:id`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>Mes films &nbsp;</>}
+                <Avatar
+                    style={{width: 30, height: 30}}
+                    alt='profile'
+                    src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && "Search..."}
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
